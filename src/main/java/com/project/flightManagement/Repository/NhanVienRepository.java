3e6f14bb775d3a8790c5b305eba3cad51a67224c
lamkbvn <lamkbvn@gmai.com>
2024-10-02 13:29:31 +0700

    lam

--- src/main/java/com/project/flightManagement/Repository/NhanVienRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.flightManagement.Repository;
-
-import com.project.flightManagement.Model.NhanVien;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface NhanVienRepository extends JpaRepository<NhanVien, Integer> {
-}
