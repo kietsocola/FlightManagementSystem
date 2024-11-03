1f9ccb344cbfeb0a747b1cd97547bd83395a440a
luthicamtri10 <luthicamtri29102004@gmail.com>
2024-11-03 19:24:31 +0700

    fix MayBay

--- src/main/java/com/project/flightManagement/Repository/QuocGiaRepository.java
+++ src/main/java/com/project/flightManagement/Repository/QuocGiaRepository.java
@@ -4,5 +4,4 @@ import com.project.flightManagement.Model.QuocGia;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuocGiaRepository extends JpaRepository<QuocGia, Integer> {
-
 }
--- src/main/java/com/project/flightManagement/Repository/ThanhPhoRepository.java
+++ src/main/java/com/project/flightManagement/Repository/ThanhPhoRepository.java
@@ -1,7 +1,11 @@
 package com.project.flightManagement.Repository;
 
+import com.project.flightManagement.Model.QuocGia;
 import com.project.flightManagement.Model.ThanhPho;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ThanhPhoRepository extends JpaRepository<ThanhPho, Integer> {
+    List<ThanhPho> findByQuocGia(QuocGia quocGia);
 }
